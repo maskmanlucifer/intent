760366b260692b11a64249808d1fd4164ab5b560
Aman Kumar Singh <maskmanaman@gmail.com>
2025-06-12 01:59:20 +0530

    fix: improve linkboard feature

--- src/components/feature-intro/index.tsx
+++ src/components/feature-intro/index.tsx
@@ -1,56 +1,82 @@
 import React, { useState } from "react";
-import { Modal, Button, Form, Input, Checkbox, Radio } from "antd";
-import { LeftOutlined, RightOutlined, SendOutlined, CloseOutlined } from "@ant-design/icons";
+import { Modal, Button } from "antd";
+import {
+  LeftOutlined,
+  RightOutlined,
+  CloseOutlined,
+} from "@ant-design/icons";
 import "./index.scss";
 
-const FeatureIntroModal = ({ visible, onClose }: { visible: boolean, onClose: () => void }) => {
+const FeatureIntroModal = ({
+  visible,
+  onClose,
+}: {
+  visible: boolean;
+  onClose: () => void;
+}) => {
   const [currentPage, setCurrentPage] = useState(0);
-  const [form] = Form.useForm();
-  const [isLoading, setIsLoading] = useState(false);
 
   const features = [
     {
       title: "Linkboard",
       description: "Access your favorite links quickly with Linkboard.",
-      media: <video controls width="860" height="480" id="linkboard-video">
-      <source
-        src="https://ik.imagekit.io/dnz8iqrsyc/linkboard-intro.mp4"
-        type="video/mp4"
-      />
-      Your browser does not support the video tag.
-    </video>,
+      media: (
+        <video controls width="860" height="480" id="linkboard-video">
+          <source
+            src="https://ik.imagekit.io/dnz8iqrsyc/linkboard-intro.mp4"
+            type="video/mp4"
+          />
+          Your browser does not support the video tag.
+        </video>
+      ),
     },
     {
       title: "Reminder",
       description: "Keep track of your tasks using our reminders.",
-      media: <video controls width="860" height="480" id="reminder-video">
-      <source
-        src="https://ik.imagekit.io/dnz8iqrsyc/reminder.mp4"
-        type="video/mp4"
-      />
-      Your browser does not support the video tag.
-    </video>,
+      media: (
+        <video controls width="860" height="480" id="reminder-video">
+          <source
+            src="https://ik.imagekit.io/dnz8iqrsyc/reminder.mp4"
+            type="video/mp4"
+          />
+          Your browser does not support the video tag.
+        </video>
+      ),
     },
     {
-        title: "Drag and Drop",
-        description: "Organize your tasks with our drag and drop feature.",
-        media: <img src="https://ik.imagekit.io/dnz8iqrsyc/drag-and-drop.gif" style={{height:'480px', width:'860px', objectFit: 'contain'}} alt="Drag and Drop" />,
+      title: "Drag and Drop",
+      description: "Organize your tasks with our drag and drop feature.",
+      media: (
+        <img
+          src="https://ik.imagekit.io/dnz8iqrsyc/drag-and-drop.gif"
+          style={{ height: "480px", width: "860px", objectFit: "contain" }}
+          alt="Drag and Drop"
+        />
+      ),
     },
     {
       title: "Shortcuts",
       description: "Access features with keyboard shortcuts.",
-      media: <img style={{height:'480px', width:'860px', objectFit: 'contain'}} src="https://ik.imagekit.io/dnz8iqrsyc/Screenshot%202025-06-09%20at%2011.56.00%E2%80%AFPM.png" alt="Shortcuts" />,
+      media: (
+        <img
+          style={{ height: "480px", width: "860px", objectFit: "contain" }}
+          src="https://ik.imagekit.io/dnz8iqrsyc/Screenshot%202025-06-09%20at%2011.56.00%E2%80%AFPM.png"
+          alt="Shortcuts"
+        />
+      ),
     },
   ];
 
-  const isLastPage = currentPage === features.length;
+  const isLastPage = currentPage === features.length - 1;
 
   const handleNext = () => {
     if (!isLastPage) {
       setCurrentPage(currentPage + 1);
       const currentMedia = features[currentPage].media;
-      if (currentMedia.type === 'video') {
-        const videoElement = document.getElementById('linkboard-video') as HTMLVideoElement;
+      if (currentMedia.type === "video") {
+        const videoElement = document.getElementById(
+          "linkboard-video"
+        ) as HTMLVideoElement;
         if (videoElement) {
           videoElement.pause();
         }
@@ -62,35 +88,15 @@ const FeatureIntroModal = ({ visible, onClose }: { visible: boolean, onClose: ()
     if (currentPage > 0) {
       setCurrentPage(currentPage - 1);
       const currentMedia = features[currentPage]?.media;
-      if (currentMedia && currentMedia.type === 'video') {
-        const videoElement = document.getElementById('linkboard-video') as HTMLVideoElement;
+      if (currentMedia && currentMedia.type === "video") {
+        const videoElement = document.getElementById(
+          "linkboard-video"
+        ) as HTMLVideoElement;
         if (videoElement) {
           videoElement.pause();
         }
       }
     }
-  };
-
-  const handleSubmit = (values: any) => {
-    setIsLoading(true);
-    fetch(
-        "https://intent-server-git-main-lzbs-projects-77777663.vercel.app/addUserFeedback",
-        {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
-          body: JSON.stringify({ feedback: JSON.stringify(values) }),
-        },
-      )
-        .then(() => {
-            onClose();
-        })
-        .catch(() => {
-            console.log("Failed to send feedback. Please try again.");
-            onClose();
-        })
-        .finally(() => {
-          setIsLoading(false);
-        });
   };
 
   return (
@@ -102,57 +108,43 @@ const FeatureIntroModal = ({ visible, onClose }: { visible: boolean, onClose: ()
       width={1000}
       className="feature-intro-modal"
       centered={true}
-      closeIcon={<CloseOutlined style={{ fontSize: '14px' }} />}
+      closeIcon={<CloseOutlined style={{ fontSize: "14px" }} />}
       maskClosable={false}
     >
       <div className="modal-header">
-        <span>Feature Tour {currentPage + 1} / {features.length + 1}</span>
+        <span>
+          Feature Tour {currentPage + 1} / {features.length}
+        </span>
       </div>
       <div className="modal-body">
-        {!isLastPage ? (
-          <>
-            <h2>{features[currentPage].title}</h2>
-            <p>{features[currentPage].description}</p>
-            <div className="media-container" key={currentPage}>{features[currentPage].media}</div>
-          </>
-        ) : (
-          <Form form={form} onFinish={handleSubmit} layout="vertical" style={{height: '558px'}}> 
-            <Form.Item
-              label={<span style={{ fontSize: '16px' }}>How useful is this feature day to day for you?</span>}
-              name="usefulness"
-              rules={[]}
-            >
-              <Radio.Group>
-                <Radio value="very">Very Useful</Radio>
-                <Radio value="somewhat">Somewhat Useful</Radio>
-                <Radio value="not">Not Useful</Radio>
-              </Radio.Group>
-            </Form.Item>
-            <Form.Item label={<span style={{ fontSize: '16px' }}>Any feature requests or suggestions?</span>} name="suggestions">
-              <Input.TextArea placeholder="Your suggestions" />
-            </Form.Item>
-            <Form.Item label={<span style={{ fontSize: '16px' }}>Email (Optional)</span>} name="email">
-              <Input placeholder="your@email.com" />
-            </Form.Item>
-            <Form.Item name="updates" valuePropName="checked">
-              <Checkbox>Receive feature updates (We promise no spam!)</Checkbox>
-            </Form.Item>
-          </Form>
-        )}
+          <h2>{features[currentPage].title}</h2>
+          <p>{features[currentPage].description}</p>
+          <div className="media-container" key={currentPage}>
+            {features[currentPage].media}
+          </div>
       </div>
       <div className="modal-footer">
-        <Button onClick={handlePrev} disabled={currentPage === 0} icon={<LeftOutlined />}>
+        <Button
+          onClick={handlePrev}
+          disabled={currentPage === 0}
+          icon={<LeftOutlined />}
+        >
           Prev
         </Button>
         {!isLastPage ? (
           <>
-            <Button type="primary" onClick={handleNext} className="last-btn" icon={<RightOutlined />}>
+            <Button
+              type="primary"
+              onClick={handleNext}
+              className="last-btn"
+              icon={<RightOutlined />}
+            >
               Next
             </Button>
           </>
         ) : (
-          <Button type="primary" onClick={() => form.submit()} className="last-btn" icon={<SendOutlined />} loading={isLoading}>
-            Submit
+          <Button type="primary" onClick={onClose} className="last-btn">
+            Finish
           </Button>
         )}
       </div>
--- src/components/linkboard/index.tsx
+++ src/components/linkboard/index.tsx
@@ -1,20 +1,37 @@
 import React from "react";
 import "./index.scss";
 import { useDispatch, useSelector } from "react-redux";
-import { removeLink, selectLinks } from "../../redux/linkboardSlice";
-import { Empty, message, Popconfirm, Select, Space, Tooltip } from "antd";
-import { ReactComponent as RemoveIcon } from "../../assets/icons/remove.svg";
+import {
+  removeLink,
+  removeLinks,
+  selectLinks,
+} from "../../redux/linkboardSlice";
+import {
+  Button,
+  Checkbox,
+  Empty,
+  message,
+  Popconfirm,
+  Select,
+  Space,
+  Tooltip,
+} from "antd";
+import { ReactComponent as TrashIcon } from "../../assets/icons/remove.svg";
 import { InfoCircleOutlined } from "@ant-design/icons";
 import { AppDispatch } from "../../redux/store";
 import { LINKBOARD_FILTER_OPTIONS } from "../../constant";
+import classNames from "classnames";
+import { TLink } from "../../types";
 
 const Linkboard = () => {
   const links = useSelector(selectLinks);
   const dispatch = useDispatch<AppDispatch>();
   const [messageApi, contextHolder] = message.useMessage();
   const [linkTypeFilter, setLinkTypeFilter] = React.useState(
-    LINKBOARD_FILTER_OPTIONS[0].value,
+    LINKBOARD_FILTER_OPTIONS[0].value
   );
+
+  const [selectedLinks, setSelectedLinks] = React.useState<string[]>([]);
 
   const filteredLinks = [...links].filter((link) => {
     if (linkTypeFilter === "all") {
@@ -22,6 +39,13 @@ const Linkboard = () => {
     }
     return link.type === linkTypeFilter;
   });
+
+  const filteredLinksMap = React.useMemo(() => {
+    return filteredLinks.reduce((acc, link) => {
+      acc[link.id] = link as TLink;
+      return acc;
+    }, {} as Record<string, TLink>);
+  }, [filteredLinks]);
 
   return (
     <div className="linkboard">
@@ -55,65 +79,154 @@ const Linkboard = () => {
         )}
         {filteredLinks.length > 0 &&
           filteredLinks.map((link) => (
-            <Tooltip title={link.url} mouseEnterDelay={0} mouseLeaveDelay={0} arrow={false} placement="left">
-            <div
-              key={link.id}
-              className="link-item"
-              onClick={() => window.open(link.url, "_blank")}
-            >
-              { link.imageUrl && (
-                <img
-                  src={
-                    link.imageUrl
-                  }
-                  alt="link"
-                />
-              )}
-              <div className="link-details">
-              {link.url && (
-                <img
-                  src={`https://www.google.com/s2/favicons?domain=${new URL(link.url).hostname}`}
-                  alt="favicon"
-                  className="favicon"
-                />
-              )}
-              {link.title && <h2 className="link-title">{link.title}</h2>}
-              {link.url && <span className="link-url">{new URL(link.url).hostname}</span>}
-                <Popconfirm
-                  icon={<InfoCircleOutlined style={{ color: "#155dfc" }} />}
-                  title="Remove url from linkboard"
-                  okText="Remove"
-                  placement="left"
-                  style={{ width: "40px" }}
-                  onCancel={(e?: React.MouseEvent<HTMLElement, MouseEvent>) => {
-                    e?.stopPropagation();
-                  }}
-                  onConfirm={(
-                    e?: React.MouseEvent<HTMLElement, MouseEvent>,
-                  ) => {
-                    e?.stopPropagation();
-                    dispatch(removeLink(link.id));
-                    messageApi.open({
-                      type: "success",
-                      content: "Link removed from linkboard",
-                      duration: 3,
-                    });
-                  }}
-                  cancelText="Cancel"
-                  description={"Are you sure you want to remove this link?"}
-                >
-                  <button
-                    className="delete-card-btn"
-                    onClick={(e) => e.stopPropagation()}
+            <Tooltip title={link.url} arrow={false} placement="top">
+              <div
+                key={link.id}
+                className="link-item"
+                onClick={() => window.open(link.url, "_blank")}
+              >
+                {link.imageUrl && <img src={link.imageUrl} alt="link" />}
+                <div className="link-details">
+                  {link.url && (
+                    <img
+                      src={`https://www.google.com/s2/favicons?domain=${new URL(link.url).hostname}`}
+                      alt="favicon"
+                      className="favicon"
+                    />
+                  )}
+                  {link.title && <h2 className="link-title">{link.title}</h2>}
+                  {link.url && (
+                    <span className="link-url">
+                      {new URL(link.url).hostname}
+                    </span>
+                  )}
+                  <Popconfirm
+                    icon={<InfoCircleOutlined style={{ color: "#155dfc" }} />}
+                    title="Remove url from linkboard"
+                    okText="Remove"
+                    placement="left"
+                    style={{ width: "40px" }}
+                    onCancel={(
+                      e?: React.MouseEvent<HTMLElement, MouseEvent>
+                    ) => {
+                      e?.stopPropagation();
+                    }}
+                    onConfirm={(
+                      e?: React.MouseEvent<HTMLElement, MouseEvent>
+                    ) => {
+                      e?.stopPropagation();
+                      dispatch(removeLink(link.id));
+                      messageApi.open({
+                        type: "success",
+                        content: "Link removed from linkboard",
+                        duration: 3,
+                      });
+                    }}
+                    cancelText="Cancel"
+                    description={"Are you sure you want to remove this link?"}
                   >
-                    <RemoveIcon className="remove-icon" />
-                  </button>
-                </Popconfirm>
+                    <button
+                      className="delete-card-btn"
+                      onClick={(e) => e.stopPropagation()}
+                    >
+                      <TrashIcon className="remove-icon" />
+                    </button>
+                  </Popconfirm>
+                </div>
+                <Checkbox
+                  onClick={(e) => e.stopPropagation()}
+                  className={classNames("link-item-select", {
+                    selected: selectedLinks.includes(link.id),
+                  })}
+                  onChange={(e) => {
+                    e.stopPropagation();
+                    e.preventDefault();
+                    setTimeout(() => {
+                      if (selectedLinks.includes(link.id)) {
+                        setSelectedLinks(
+                          selectedLinks.filter((id) => id !== link.id)
+                        );
+                      } else {
+                        setSelectedLinks([...selectedLinks, link.id]);
+                      }
+                    }, 50);
+                  }}
+                  checked={selectedLinks.includes(link.id)}
+                />
               </div>
-            </div>
             </Tooltip>
           ))}
       </div>
+      {selectedLinks.length > 0 && (
+        <div className="selected-links">
+          <div className="selected-links-header">
+            <h1>
+              <span className="selected-links-count">
+                {selectedLinks.length}
+              </span>{" "}
+              link{selectedLinks.length > 1 ? "s" : ""} selected
+            </h1>
+          </div>
+          <div className="selected-links-body">
+            <Button size="small" danger onClick={() => setSelectedLinks([])}>
+              Deselect all
+            </Button>
+            <Popconfirm
+              title="Remove links from linkboard"
+              okText="Remove"
+              placement="top"
+              style={{ width: "40px" }}
+              arrow={false}
+              onConfirm={() => {
+                dispatch(removeLinks(selectedLinks));
+                setSelectedLinks([]);
+                messageApi.open({
+                  type: "success",
+                  content: "Links removed from linkboard",
+                  duration: 3,
+                });
+              }}
+            >
+              <Button type="primary" size="small" danger>
+                Delete {selectedLinks.length} link{selectedLinks.length > 1 ? "s" : ""}
+              </Button>
+            </Popconfirm>
+          </div>
+          <div className="selected-links-footer">
+            {selectedLinks.map((link) => {
+              const linkData = filteredLinksMap[link];
+              return (
+                <div key={link} className="selected-link">
+                  {linkData.url && (
+                    <img
+                      src={`https://www.google.com/s2/favicons?domain=${new URL(linkData.url).hostname}`}
+                      alt="favicon"
+                      className="favicon"
+                    />
+                  )}
+                  {linkData.url && (
+                    <span className="link-host">
+                      {new URL(linkData.url).hostname}
+                    </span>
+                  )}
+                  {linkData.url && (
+                    <span className="link-url">{linkData.url}</span>
+                  )}
+                  <TrashIcon
+                    className="remove-icon-bucket"
+                    onClick={(e) => {
+                      e.stopPropagation();
+                      setSelectedLinks(
+                        selectedLinks.filter((id) => id !== link)
+                      );
+                    }}
+                  />
+                </div>
+              );
+            })}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
